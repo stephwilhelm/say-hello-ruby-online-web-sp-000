8e4cdb97361d930b788e160c7966f21f690b46d7
Steph W <wilhelm.steph@gmail.com>
2020-03-16 19:04:31 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,11 @@
+<<<<<<< HEAD
 
 # Build your say_hello method here
 def say_hello(name = "Ruby Programmer")
   puts "Hello #{name}!"
 end
+=======
+# Build your say_hello method here
+def say_hello(name = "Ruby Programmer")
+  puts "Hello #{name}!"
+>>>>>>> 515b52cc8891ca15ad972ae6ee58c5346d1fcc97
